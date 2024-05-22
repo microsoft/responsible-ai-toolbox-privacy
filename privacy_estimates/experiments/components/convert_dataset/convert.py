2ab5a060e4f98a2adebc52ae4e9e38a2cff6a7d7
Lukas Wutschitz <luwutsch@microsoft.com>
2024-05-22 11:52:10 +0000

    Fix env

--- privacy_estimates/experiments/components/convert_dataset/convert.py
+++ privacy_estimates/experiments/components/convert_dataset/convert.py
@@ -2,16 +2,16 @@ from mldesigner import command_component, Input, Output
 from datasets import Dataset
 
 
-@command_component()
-def convert_jsonl_to_hf(data: Input(type="uri_file"), output: Output(type="uri_folder")):
+@command_component(environment="environment.aml.yaml")
+def convert_jsonl_to_hfd(data: Input(type="uri_file"), output: Output(type="uri_folder")):
     """
     Convert a JSONL file to a Hugging Face dataset.
     """
     Dataset.from_json(data).save_to_disk(output)
 
 
-@command_component()
-def convert_hf_to_jsonl(data: Input(type="uri_folder"), output: Output(type="uri_file")):
+@command_component(environment="environment.aml.yaml")
+def convert_hfd_to_jsonl(data: Input(type="uri_folder"), output: Output(type="uri_file")):
     """
     Convert a Hugging Face dataset to a JSONL file.
     """
