215abfb33a05f09c5b00910f5699379b2ff3fae3
Lukas Wutschitz <luwutsch@microsoft.com>
2024-05-28 14:45:36 +0000

    Add TODO for later

--- privacy_estimates/experiments/components/convert_uri_file_to_primitive/convert_uri_file.py
+++ privacy_estimates/experiments/components/convert_uri_file_to_primitive/convert_uri_file.py
@@ -1,5 +1,8 @@
 from mldesigner import command_component, Input
 
+# TODO: This could be also solved by assigning a dict with keys conda_file and image to the environment parameter
+# This should avoid many warnings about componentenvironment name not being able to set
+
 @command_component(environment="environment.aml.yaml")
 def convert_uri_file_to_int(uri_file: Input(type="uri_file")) -> int:
     with open(uri_file, 'r') as file:
