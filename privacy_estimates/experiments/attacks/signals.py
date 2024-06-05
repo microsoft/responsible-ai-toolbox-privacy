a95783b83817d791182812f9c058c3ada5fb3740
Lukas Wutschitz <luwutsch@microsoft.com>
2024-06-05 15:04:22 +0000

    Rename to attention mask to completion mask since this is used to indicate where the losses will be computed rather than just where there are valid tokens

--- privacy_estimates/experiments/attacks/signals.py
+++ privacy_estimates/experiments/attacks/signals.py
@@ -23,7 +23,7 @@ class PredictionFormat(Enum):
 
 class Signal(ABC):
     def compute_mi_signal_from_logits(self, logits: np.ndarray, labels: np.ndarray,
-                                      attention_mask: Optional[np.ndarray] = None):
+                                      completion_mask: Optional[np.ndarray] = None):
         """
         Compute the membership inference signal from the logits and labels.
 
@@ -34,40 +34,46 @@ class Signal(ABC):
         Args:
             logits: The logits of the model.
             labels: The true labels of the data.
+            completion_mask: The mask for the completion of the sequence i.e. which tokens will be considered to compute the
+                             signal. If None, all elements are considered.
         """
         raise NotImplementedError("This method should be implemented by the subclass")
 
     def compute_mi_signal_from_logprobs(self, logprobs: np.ndarray, labels: np.ndarray,
-                                        attention_mask: Optional[np.ndarray] = None) -> np.ndarray:
+                                        completion_mask: Optional[np.ndarray]) -> np.ndarray:
         """
         Compute the membership inference signal from the log probabilities and labels.
 
         Args:
             logprobs: The log probabilities of the model.
             labels: The true labels of the data.
+            completion_mask: The mask for the completion of the sequence i.e. which tokens will be considered to compute the
+                             signal. If None, all elements are considered.
         """
         raise NotImplementedError("This method should be implemented by the subclass")
     
     def compute_mi_signal_from_probs(self, probs: np.ndarray, labels: np.ndarray,
-                                     attention_mask: Optional[np.ndarray]) -> np.ndarray:
+                                     completion_mask: Optional[np.ndarray]) -> np.ndarray:
         """
         Compute the membership inference signal from the probabilities and labels.
 
         Args:
             probs: The probabilities of the model.
             labels: The true labels of the data.
+            completion_mask: The mask for the completion of the sequence i.e. which tokens will be considered to compute the
+                             signal. If None, all elements are considered.
         """
         raise NotImplementedError("This method should be implemented by the subclass")
 
-    def assert_inputs_valid(self, logits: np.ndarray, labels: np.ndarray, attention_mask: Optional[np.ndarray]):
+    def assert_inputs_valid(self, logits: np.ndarray, labels: np.ndarray, completion_mask: np.ndarray):
         assert logits.ndim > 1
         assert np.abs(logits.sum(axis=-1)-1).max() > 1e-3, "Logits should be unnormalized, i.e., not softmaxed"
         assert np.abs(np.exp(logits).sum(axis=-1)-1).max() > 1e-3, (
             "log(logits) should be unnormalized, make sure you are not using logprobs"
         )
         assert labels.ndim == logits.ndim - 1
-        assert labels[attention_mask].max() < logits.shape[-1]
-        assert labels[attention_mask].min() >= 0
+        assert labels[completion_mask].max() < logits.shape[-1]
+        assert labels[completion_mask].min() >= 0
 
 
 class TaylorSoftMargin(Signal):
@@ -77,13 +83,15 @@ class TaylorSoftMargin(Signal):
         self.temp = temp
 
     def compute_mi_signal_from_logits(self, logits: np.ndarray, labels: np.ndarray,
-                                      attention_mask: Optional[np.ndarray] = None) -> np.ndarray:
+                                      completion_mask: Optional[np.ndarray] = None) -> np.ndarray:
         """
         For large `taylor_n` the return values are in [0,1]
         Args:
             logits: The logits of the model. 2 or 3 dimensional tensor.
                     Shape of [n_samples, (seq_len,) n_classes] where seq_len could be 0
-
+            labels: The true labels of the data. Shape of [n_samples, (seq_len)]
+            completion_mask: The mask for the completion of the sequence i.e. which tokens will be considered to compute the
+                             signal. If None, all elements are considered.
         Returns:
             The signal for the membership inference attack. of shape [n_samples, (seq_len)]
         """
@@ -91,7 +99,7 @@ class TaylorSoftMargin(Signal):
             attention_mask = np.ones_like(labels)
         attention_mask = attention_mask.astype(bool)
         labels[~attention_mask] = 0
-        self.assert_inputs_valid(logits=logits, labels=labels, attention_mask=attention_mask)
+        self.assert_inputs_valid(logits=logits, labels=labels, completion_mask=completion_mask)
         logit_signals = logits/self.temp
         taylor_logits = get_taylor(logit_signals, self.n)
         taylor_logit_sum = taylor_logits.sum(axis=-1)
@@ -101,7 +109,7 @@ class TaylorSoftMargin(Signal):
         soft_taylor_true_logit = get_taylor(true_logit - self.m, self.n)
         taylor_logit_sum = taylor_logit_sum + soft_taylor_true_logit
         signal = (soft_taylor_true_logit / taylor_logit_sum)
-        signal[~attention_mask] = np.nan
+        signal[~completion_mask] = np.nan
         return signal
     
 
@@ -113,20 +121,22 @@ class CrossEntropy(Signal):
         self.compute_loss = CrossEntropyLoss(ignore_index=self.ignored_index, reduction="none")
 
     def compute_mi_signal_from_logits(self, logits: np.ndarray, labels: np.ndarray,
-                                      attention_mask: Optional[np.ndarray] = None) -> np.ndarray:
+                                      completion_mask: Optional[np.ndarray] = None) -> np.ndarray:
         """
         Args:
             logits: The logits of the model. 2 or 3 dimensional tensor.
                     Shape of [n_samples, (seq_len,) n_classes] where seq_len could be 0
-
+            labels: The true labels of the data. Shape of [n_samples, (seq_len)]
+            completion_mask: The mask for the completion of the sequence i.e. which tokens will be considered to compute the
+                             signal. If None, all elements are considered.
         Returns:
             The signal for the membership inference attack. of shape [n_samples, (seq_len)]
         """
-        if attention_mask is None:
-            attention_mask = np.ones_like(labels)
-        attention_mask = attention_mask.astype(bool)
-        labels[~attention_mask] = self.ignored_index
-        self.assert_inputs_valid(logits=logits, labels=labels, attention_mask=attention_mask)
+        if completion_mask is None:
+            completion_mask = np.ones_like(labels)
+        completion_mask = completion_mask.astype(bool)
+        labels[~completion_mask] = self.ignored_index
+        self.assert_inputs_valid(logits=logits, labels=labels, completion_mask=completion_mask)
         logits = logits/self.temp
 
         # pytorch expects n_classes dimension as the 2nd (i.e. index 1) dimension
@@ -136,7 +146,7 @@ class CrossEntropy(Signal):
         import torch
         signal = -self.compute_loss(torch.tensor(logits), torch.tensor(labels)).numpy()
 
-        signal[~attention_mask] = np.nan
+        signal[~completion_mask] = np.nan
         return signal
     
 
@@ -144,18 +154,18 @@ SIGNALS = {cls.__name__: cls for cls in Signal.__subclasses__()}
 
 
 def compute_mi_signals(predictions: np.ndarray, labels: np.ndarray, method: str, prediction_format: PredictionFormat,
-                       attention_mask: Optional[np.ndarray] = None, **kwargs) -> np.ndarray:
+                       completion_mask: Optional[np.ndarray] = None, **kwargs) -> np.ndarray:
     if method not in SIGNALS:
         raise ValueError(f"Method {method} not found. Available methods: {SIGNALS.keys()}")
     signal_method = SIGNALS[method](**kwargs)
     if prediction_format == PredictionFormat.LOGIT:
         return signal_method.compute_mi_signal_from_logits(predictions=predictions, labels=labels,
-                                                           attention_mask=attention_mask)
+                                                           completion_mask=completion_mask)
     elif prediction_format == PredictionFormat.LOGPROB:
         return signal_method.compute_mi_signal_from_logprobs(predictions=predictions, labels=labels,
-                                                             attention_mask=attention_mask)
+                                                             completion_mask=completion_mask)
     elif prediction_format == PredictionFormat.PROB:
         return signal_method.compute_mi_signal_from_probs(predictions=predictions, labels=labels,
-                                                          attention_mask=attention_mask)
+                                                          completion_mask=completion_mask)
     else:
         raise ValueError(f"Prediction format {prediction_format} not found. Available formats: {PredictionFormat}")
